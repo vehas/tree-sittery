66d9c4cd4ec236f93e466353bdea1f39c202db58
Veha Suwatphisankij <vehas@users.noreply.github.com>
2022-08-15 21:35:45 +0700

    remove older tree-sitter-graphql version

--- tree-sitter-graphql/index.js
+++
@@ -1,9 +0,0 @@
-try {
-  module.exports = require("./build/Release/tree_sitter_graphql");
-} catch (error) {
-  try {
-    module.exports = require("./build/Debug/tree_sitter_graphql");
-  } catch (_) {
-    throw error
-  }
-}
