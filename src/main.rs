5810afc77ecd5ca9734e4ccd2d1cbb998cc074fa
Veha Suwatphisankij <vehas@users.noreply.github.com>
2022-08-15 21:35:26 +0700

    add tree sitter parser used

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use tree_sitter::{Parser, Language};
+
+extern "C" { fn tree_sitter_graphql() -> Language; }
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_parser() {
+    let language = unsafe { tree_sitter_graphql() };
+    let mut parser = Parser::new();
+    parser.set_language(language).unwrap();
+
+    let source_code = "type a{b: String}";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    assert_eq!(tree.root_node().to_sexp(), "(source_file (module_declaration (module_header (module_keyword) (module_identifier (simple_identifier))) (module_nonansi_header (list_of_ports))))");
 }
